3d4867cedae76d9977310dba3bdb51df1dabe92a
Ben Coleman <benc.uk@gmail.com>
2026-04-06 16:22:03 +0100

    add t to track time

--- examples/basic1/src/main.rs
+++ examples/basic1/src/main.rs
@@ -1,24 +1,22 @@
 use argh::colour::{BLACK, Colour};
 use argh::core::{Engine, Scene};
 
-struct MyScene {
-    t: f64,
-}
+struct MyScene {}
 
 const W: usize = 1024;
 const H: usize = 768;
 
 impl Scene for MyScene {
     // You must always implement the update method it will be called once per frame
-    fn update(&mut self, engine: &mut Engine, dt: f64) {
-        self.t += dt;
-        let r = (self.t * 255.0 * 2.0 % 255.0) as u8;
+    fn update(&mut self, engine: &mut Engine, _: f64) {
+        let r = (engine.t() * 255.0 * 2.0 % 255.0) as u8;
 
         engine.clear(BLACK);
 
         for y in 0..H {
             for x in 0..W {
-                let c2 = Colour::new(r, (x * 3 % 255) as u8, (y * 3 % 255) as u8);
+                let mut c2 = Colour::new(r, (x * 3 % 255) as u8, (y * 3 % 255) as u8);
+                c2.scale(engine.t() * 2.0 % 2.0);
                 engine.set_pixel(x, y, c2);
             }
         }
@@ -27,6 +25,6 @@ impl Scene for MyScene {
 
 fn main() {
     let e = Engine::new(W, H, String::from("Argh: modules/basic1"));
-    let s = MyScene { t: 0.0 };
+    let s = MyScene {};
     e.start(s);
 }
