e47e911b40e1d637835d18626fc45f9dd231fd94
Ben Coleman <benc.uk@gmail.com>
2026-04-06 15:53:33 +0100

    add example to docs

--- argh/src/lib.rs
+++ argh/src/lib.rs
@@ -1,5 +1,22 @@
 //! Argh (Another Rust Graphics Helper) is a software rendering library using minifb. It supports 2D and 3D graphics functions and has been developed without AI assistance
-
+///
+/// # Examples
+/// Minimal usage and getting started
+/// ```
+/// use argh::core::{Engine, Scene};
+///
+/// struct MyScene {}
+/// impl Scene for MyScene {
+///     fn update(&mut self, e: &mut Engine, _: f64) {
+///         // Update & draw here
+///     }
+/// }
+///
+/// fn main() {
+///     let eng = Engine::new(800, 600, String::from("Hello World"));
+///     eng.start(MyScene {});
+/// }
+/// ```
 pub(crate) mod buffer;
 pub mod colour;
 pub mod core;
